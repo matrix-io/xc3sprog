dc6479cae12b85e150b765b7679de6ce04eb3397
bonnes <bonnes@5e3be8be-922f-45f9-ad52-57b51d30232e>
2008-12-20 17:37:49 +0000

    Cosmetic: Rename constants with names from the 1532 BSDL file

--- progalgxcf.cpp
+++ progalgxcf.cpp
@@ -22,19 +22,19 @@ Dmitry Teytelman [dimtey@gmail.com] 14 Jun 2006 [applied 13 Aug 2006]:
 */
 
 
-
+ 
 
 #include "progalgxcf.h"
 
 const byte ProgAlgXCF::SERASE=0x0a;
-const byte ProgAlgXCF::ISPEN=0xe8;
-const byte ProgAlgXCF::FPGM=0xea;
-const byte ProgAlgXCF::FADDR=0xeb;
-const byte ProgAlgXCF::FERASE=0xec;
-const byte ProgAlgXCF::FDATA0=0xed;
+const byte ProgAlgXCF::ISC_ENABLE=0xe8;
+const byte ProgAlgXCF::ISC_PROGRAM=0xea;
+const byte ProgAlgXCF::ISC_ADDRESS_SHIFT=0xeb;
+const byte ProgAlgXCF::ISC_ERASE=0xec;
+const byte ProgAlgXCF::ISC_DATA_SHIFT=0xed;
 const byte ProgAlgXCF::CONFIG=0xee;
-const byte ProgAlgXCF::FVFY0=0xef;
-const byte ProgAlgXCF::NORMRST=0xf0;
+const byte ProgAlgXCF::ISC_READ=0xef;
+const byte ProgAlgXCF::ISC_DISABLE=0xf0;
 const byte ProgAlgXCF::IDCODE=0xfe;
 const byte ProgAlgXCF::BYPASS=0xff;
 
@@ -51,7 +51,7 @@ ProgAlgXCF::ProgAlgXCF(Jtag &j, IOBase &i, int bs)
 int ProgAlgXCF::erase()
 {
   byte data[4];
-  jtag->shiftIR(&NORMRST);
+  jtag->shiftIR(&ISC_DISABLE);
   io->cycleTCK(40000);
   byte ircap[1];
   jtag->shiftIR(&BYPASS,ircap);
@@ -59,14 +59,14 @@ int ProgAlgXCF::erase()
     fprintf(stderr,"Device is write protected.\n");
     return 1;
   }
-  jtag->shiftIR(&ISPEN);
-  jtag->shiftIR(&FADDR);
+  jtag->shiftIR(&ISC_ENABLE);
+  jtag->shiftIR(&ISC_ADDRESS_SHIFT);
   jtag->longToByteArray(1,data);
   jtag->shiftDR(data,0,16);
   io->cycleTCK(2);
   
   printf("Erasing...."); fflush(stdout);
-  jtag->shiftIR(&FERASE);
+  jtag->shiftIR(&ISC_ERASE);
   io->cycleTCK(2400000);
   printf("done.\n");
 
@@ -78,18 +78,18 @@ int ProgAlgXCF::erase()
 
 int ProgAlgXCF::program(BitFile &file)
 {
-  jtag->shiftIR(&NORMRST);
+  jtag->shiftIR(&ISC_DISABLE);
   io->cycleTCK(40000);
   io->setTapState(IOBase::TEST_LOGIC_RESET);
   byte data[4];
-  jtag->shiftIR(&ISPEN);
+  jtag->shiftIR(&ISC_ENABLE);
   data[0]=0x34;
   jtag->shiftDR(data,0,6);
 
   for(unsigned int i=0; i<file.getLength(); i+=block_size){
     int frame=i/(block_size/32);
     printf("Programming frames 0x%04x to 0x%04x....",frame,frame+31); fflush(stdout);
-    jtag->shiftIR(&FDATA0);
+    jtag->shiftIR(&ISC_DATA_SHIFT);
     if((i+block_size)<=file.getLength()){
       jtag->shiftDR(&(file.getData())[i/8],0,block_size);
     }
@@ -101,10 +101,10 @@ int ProgAlgXCF::program(BitFile &file)
       jtag->shiftDR(paddata,0,pad*8);
     }
     jtag->longToByteArray(frame,data);
-    jtag->shiftIR(&FADDR);
+    jtag->shiftIR(&ISC_ADDRESS_SHIFT);
     jtag->shiftDR(data,0,16);
     io->cycleTCK(2);
-    jtag->shiftIR(&FPGM);
+    jtag->shiftIR(&ISC_PROGRAM);
     io->cycleTCK(5000);
     printf("done.\n");
   } 
