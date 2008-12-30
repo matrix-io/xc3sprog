afcacd65b0194b4771db49fcbf9b3a332be6bcc6
bonnes <bonnes@5e3be8be-922f-45f9-ad52-57b51d30232e>
2008-12-30 16:17:03 +0000

    USB transactions must be as bigh as possible. Expose a txrx_block functions to allow optimazations
    git-svn-id: http://svn.code.sf.net/p/xc3sprog/code/trunk@40 5e3be8be-922f-45f9-ad52-57b51d30232e

--- iobase.cpp
+++ iobase.cpp
@@ -37,24 +37,7 @@ using namespace std;
 void IOBase::shiftTDITDO(const unsigned char *tdi, unsigned char *tdo, int length, bool last)
 {
   if(length==0) return;
-
-  int i=0;
-  int j=0;
-  unsigned char tdo_byte=0;
-  unsigned char tdi_byte=tdi[j];
-  while(i<length-1){
-    tdo_byte=tdo_byte+(txrx(false, (tdi_byte&1)==1)<<(i%8));
-    tdi_byte=tdi_byte>>1;
-    i++;
-    if((i%8)==0){ // Next byte
-      tdo[j]=tdo_byte; // Save the TDO byte
-      tdo_byte=0;
-      j++;
-      tdi_byte=tdi[j]; // Get the next TDI byte
-    }
-  };
-  tdo_byte=tdo_byte+(txrx(last, (tdi_byte&1)==1)<<(i%8)); // TMS set if last=true
-  tdo[j]=tdo_byte;
+  txrx_block(tdi, tdo, length,last);
   nextTapState(last); // If TMS is set the the state of the tap changes
   return;
 }
