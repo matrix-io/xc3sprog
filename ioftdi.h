f7dfd299db40fc3bad4e5727c0d7cd3113b9c000
bonnes <bonnes@5e3be8be-922f-45f9-ad52-57b51d30232e>
2009-01-05 10:38:56 +0000

    Code alternate path for libftd2xx, clean up for Win32 compile
    git-svn-id: http://svn.code.sf.net/p/xc3sprog/code/trunk@52 5e3be8be-922f-45f9-ad52-57b51d30232e

--- ioftdi.h
+++ ioftdi.h
@@ -21,10 +21,53 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 #ifndef IOFTDI_H
 #define IOFTDI_H
 
-#if defined ( __MINGW)||(__MINGW32__)
+#if defined (__WIN32__)
+#include <windows.h>
+/// MPSSE bitbang modes
+enum ftdi_mpsse_mode {
+    BITMODE_RESET  = 0x00,
+    BITMODE_BITBANG= 0x01,
+    BITMODE_MPSSE  = 0x02,
+    BITMODE_SYNCBB = 0x04,
+    BITMODE_MCU    = 0x08,
+    // CPU-style fifo mode gets set via EEPROM
+    BITMODE_OPTO   = 0x10,
+    BITMODE_CBUS   = 0x20
+};
+/* Shifting commands IN MPSSE Mode*/
+#define MPSSE_WRITE_NEG 0x01   /* Write TDI/DO on negative TCK/SK edge*/
+#define MPSSE_BITMODE   0x02   /* Write bits, not bytes */
+#define MPSSE_READ_NEG  0x04   /* Sample TDO/DI on negative TCK/SK edge */
+#define MPSSE_LSB       0x08   /* LSB first */
+#define MPSSE_DO_WRITE  0x10   /* Write TDI/DO */
+#define MPSSE_DO_READ   0x20   /* Read TDO/DI */
+#define MPSSE_WRITE_TMS 0x40   /* Write TMS/CS */
+
+/* FTDI MPSSE commands */
+#define SET_BITS_LOW   0x80
+/*BYTE DATA*/
+/*BYTE Direction*/
+#define SET_BITS_HIGH  0x82
+/*BYTE DATA*/
+/*BYTE Direction*/
+#define GET_BITS_LOW   0x81
+#define GET_BITS_HIGH  0x83
+#define LOOPBACK_START 0x84
+#define LOOPBACK_END   0x85
+#define TCK_DIVISOR    0x86
+/* Commands in MPSSE and Host Emulation Mode */
+#define SEND_IMMEDIATE 0x87 
+#define WAIT_ON_HIGH   0x88
+#define WAIT_ON_LOW    0x89
+
+
 #else
-#include <usb.h>
 #include <ftdi.h>
+#include <usb.h>
+#endif
+
+#if defined USE_FTD2XX
+#include <ftd2xx.h>
 #endif
 
 #include "iobase.h"
@@ -40,13 +83,19 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 class IOFtdi : public IOBase
 {
  protected:
-#if defined ( __MINGW)||(__MINGW32__)
+#if defined (USE_FTD2XX)
+  FT_HANDLE ftdi;   
 #else
   struct ftdi_context ftdi;
 #endif
   unsigned char usbuf[TX_BUF];
-  int buflen, bptr, total, calls;
-  int subtype;
+  int buflen;
+#if defined(USE_FTD2XX)
+  DWORD bptr;
+#else
+  int bptr;
+#endif
+  int total, calls,subtype;
 
  public:
   IOFtdi(int vendor, int product, char const *desc, char const *serial, int subtype);
