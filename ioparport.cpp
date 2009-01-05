880660b4744ebe040031787be48d147ef2c54405
bonnes <bonnes@5e3be8be-922f-45f9-ad52-57b51d30232e>
2009-01-05 10:40:11 +0000

    ioparport: txrx_block must cope with tdi == NULL too

--- ioparport.cpp
+++ ioparport.cpp
@@ -384,17 +384,22 @@ void IOParport::txrx_block(const unsigned char *tdi, unsigned char *tdo, int len
   int i=0;
   int j=0;
   unsigned char tdo_byte=0;
-  unsigned char tdi_byte=tdi[j];
+  unsigned char tdi_byte;
+  if (tdi)
+      tdi_byte = tdi[j];
+      
   while(i<length-1){
-    tdo_byte=tdo_byte+(txrx(false, (tdi_byte&1)==1)<<(i%8));
-    tdi_byte=tdi_byte>>1;
+      tdo_byte=tdo_byte+(txrx(false, (tdi_byte&1)==1)<<(i%8));
+      if (tdi)
+	  tdi_byte=tdi_byte>>1;
     i++;
     if((i%8)==0){ // Next byte
 	if(tdo)
 	    tdo[j]=tdo_byte; // Save the TDO byte
       tdo_byte=0;
       j++;
-      tdi_byte=tdi[j]; // Get the next TDI byte
+      if (tdi)
+	  tdi_byte=tdi[j]; // Get the next TDI byte
     }
   };
   tdo_byte=tdo_byte+(txrx(last, (tdi_byte&1)==1)<<(i%8)); // TMS set if last=true
