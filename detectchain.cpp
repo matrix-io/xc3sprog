3c602c5d67801dfbf5fd7faf0fdf77bbd80ae26e
bonnes <bonnes@5e3be8be-922f-45f9-ad52-57b51d30232e>
2009-05-11 10:38:45 +0000

    Rearrange code to clarify the strcmp
    git-svn-id: http://svn.code.sf.net/p/xc3sprog/code/trunk@174 5e3be8be-922f-45f9-ad52-57b51d30232e

--- detectchain.cpp
+++ detectchain.cpp
@@ -147,16 +147,16 @@ args_done:
     io->setVerbose(verbose);
   }
   catch(io_exception& e) {
-    if(strcmp(cable, "pp")) {
-      if(!dev)  dev = "*";
-      fprintf(stderr, "Could not access USB device (%s).\n", dev);
-    }
-    else {
+    if(strcmp(cable, "pp") == 0) {
       fprintf(stderr,"Could not access parallel device '%s'.\n", dev);
       fprintf(stderr,"You may need to set permissions of '%s' \n", dev);
       fprintf(stderr,
               "by issuing the following command as root:\n\n# chmod 666 %s\n\n",
               dev);
+    }
+    else {
+      if(!dev)  dev = "*";
+      fprintf(stderr, "Could not access USB device (%s).\n", dev);
     }
     return 1;
   }
