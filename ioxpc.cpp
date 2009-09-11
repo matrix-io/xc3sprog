6a3537b3ac4491ada9641b0ecc8ded0b377c67a2
u_bonnes <u_bonnes@5e3be8be-922f-45f9-ad52-57b51d30232e>
2009-09-11 12:57:13 +0000

    Set the configuration and claim the interface for XPC
    git-svn-id: http://svn.code.sf.net/p/xc3sprog/code/trunk@336 5e3be8be-922f-45f9-ad52-57b51d30232e

--- ioxpc.cpp
+++ ioxpc.cpp
@@ -594,6 +594,23 @@ int IOXPC::xpc_usb_open_desc(int vendor, int product, const char* description,
 		      continue;
 		    }
 		}
+	      if (usb_set_configuration (xpcu, dev->config[0].bConfigurationValue) < 0)
+		{
+		  fprintf (stderr, "%s: usb_set_configuration: failed conf %d\n",
+			   __FUNCTION__, dev->config[0].bConfigurationValue);
+		  fprintf (stderr, "%s\n", usb_strerror());
+		  usb_close (xpcu);
+		  xpc_error_return(-10, 
+				       "unable to set configuration");
+		}
+	      if (usb_claim_interface (xpcu, 0) < 0){
+		fprintf (stderr, "%s:usb_claim_interface: failed interface 0\n",
+			 __FUNCTION__);
+		fprintf (stderr, "%s\n", usb_strerror());
+		usb_close (xpcu);
+		xpc_error_return(-11, 
+				       "unable to claim interface");
+	      }
 	      return 0;
 	    }
 	}
