d8e9d1f701e9cf83be3caf0eac1221cc2a03061d
u_bonnes <u_bonnes@5e3be8be-922f-45f9-ad52-57b51d30232e>
2009-10-01 13:28:58 +0000

    Move cable setting and detection into a utility function
    git-svn-id: http://svn.code.sf.net/p/xc3sprog/code/trunk@388 5e3be8be-922f-45f9-ad52-57b51d30232e

--- utilities.h
+++ utilities.h
@@ -1,1 +1,2 @@
 unsigned int detect_chain(Jtag &jtag, DeviceDB &db);
+int getIO(std::auto_ptr<IOBase> *io, char const *cable, int subtype, int  vendor, int  product, char const *dev, char const *desc, char const *serial);
