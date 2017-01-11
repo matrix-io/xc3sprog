39009f965f7e2a474ebe6bd4b10ec85a1bc204c8
Louie Caulfield <louie.caulfield@gmail.com>
2017-01-11 12:58:51 +0100

    update build script

--- bscan_spi/vivado/build.sh
+++ bscan_spi/vivado/build.sh
@@ -1,6 +1,6 @@
-PART=xc7a200t
-PACKAGE=fbg676
+PART=xc7a35t
+PACKAGE=cpg236
 
 SCRIPT_PATH=`dirname $0`
-VIVADO_EXEC=~/Xilinx/Vivado/2015.4/bin/vivado
+VIVADO_EXEC=/opt/Xilinx/Vivado/2016.2/bin/vivado
 ${VIVADO_EXEC} -mode batch -source ${SCRIPT_PATH}/build.tcl -tclargs ${PART} ${PACKAGE}
